4fdb955bf71e8b4ce054b86c4ea380e99cfe3939
Thomas <thomasfire@yandex.ru>
2016-06-22 14:26:11 +0300

    Added writing user's DB and getting messages in readable format

--- getmsg.py
+++ getmsg.py
@@ -7,6 +7,24 @@ import os
 import shutil
 import sys
 import re
+import datetime
+
+def getname(user_id,vk_session):
+        db=open("vk_users.db","r")
+        users=db.read()
+        if str(user_id) in users:
+            users=users.split("\n")
+            for x in users:
+                if str(user_id) in x:
+                    return x.split(':')[1]
+        else:
+            db.close()
+            vk = vk_session.get_api()
+            name=vk.users.get(user_ids=user_id)
+            db=open("vk_users.db","a")
+            db.write(str(user_id)+":"+" ".join([name[0]['first_name'],name[0]['last_name']])+"\n")
+            db.close()
+            return " ".join([name[0]['first_name'],name[0]['last_name']])
 
 
 def main():
@@ -28,8 +46,10 @@ def main():
 
     tools = vk_api.VkTools(vk_session)
     msgs = tools.get_all('messages.get',1,values={'count': 1, 'chat_id': chatid},limit=1)
-    print(str(msgs['items'][0]['user_id'])+"  "+str(msgs['items'][0]['date'])+' : '+msgs['items'][0]['body'])
-
+    for x in range(10):
+        print(getname(msgs['items'][x]['user_id'],vk_session)+"  "+
+        datetime.datetime.fromtimestamp(msgs['items'][x]['date']).strftime('%Y-%m-%d %H:%M:%S')+' : '+msgs['items'][x]['body'])
+        #print(getname(msgs['items'][x]['user_id'],vk_session))
 
 if __name__ == '__main__':
     main()
