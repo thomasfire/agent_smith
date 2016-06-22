3f8399b4ba7a4b9e6e04ff2d784b5ecad462c1e5
Thomas <thomasfire@yandex.ru>
2016-06-22 15:01:49 +0300

    Fixing bugs and adding forwarded messages

--- getmsg.py
+++ getmsg.py
@@ -46,16 +46,15 @@ def main():
 
     tools = vk_api.VkTools(vk_session)
     msgs = tools.get_all('messages.get',1,values={'count': 1, 'chat_id': chatid},limit=1)
-    for x in range(10):
-        print(getname(msgs['items'][x]['user_id'],vk_session)+"  "+
-        datetime.datetime.fromtimestamp(msgs['items'][x]['date']).strftime('%Y-%m-%d %H:%M:%S')+' : '+msgs['items'][x]['body'])
+    for x in msgs['items']:
+        print(getname(x['user_id'],vk_session)+"  "+
+        datetime.datetime.fromtimestamp(x['date']).strftime('%Y-%m-%d %H:%M:%S')+' : '+x['body'])
+        if 'fwd_messages' in x.keys():
+            for y in x['fwd_messages']:
+                print(" forwarded from "+getname(y['user_id'],vk_session)+" "+
+                datetime.datetime.fromtimestamp(y['date']).strftime('%Y-%m-%d %H:%M:%S')+
+                " : "+y['body'])
         #print(getname(msgs['items'][x]['user_id'],vk_session))
 
 if __name__ == '__main__':
     main()
-
-
-
-def startuploading(resource):
-    print("Uploading....")
-    main(resource)
