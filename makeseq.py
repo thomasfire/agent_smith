a6befbab8d5cb2af0ba13764b427ae3fbf258797
Thomas <thomasfire@yandex.ru>
2016-06-22 21:53:00 +0300

    Making sequense to send from messages

---
+++ makeseq.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+#sorts and makes sequance
+#also cleans up msgshistory.db
+
+
+
+def main():
+    histmsg=open('msgshistory.db','r')
+    f=open('keywords.db','r')
+    keywords=f.read().split()
+    listofmsgs=histmsg.read().split(';\n@')
+    histmsg.close()
+    f.close()
+    impnt=[]
+    allmsg=[]
+
+    for x in range(len(listofmsgs)):
+        listofmsgs[x]=listofmsgs[x].strip('@ ').strip(';')
+
+    f=open('msgs.sent','r')
+    sent=f.read()
+    f.close
+    f=open('msgs.seq','w')
+
+    for x in listofmsgs:
+        msg=x.split(' : ')
+        if msg[0].strip() not in sent:
+            allmsg.append(msg[0].strip())
+            for y in keywords:
+                if y in msg[2].lower() or msg[2]==msg[2].upper():
+                    impnt.append(msg[0].strip())
+                    break
+    f.write("important:{"+" ".join(impnt)+"}\n\n" +"all:{"+" ".join(allmsg)+"}")
+
+
+if __name__ == '__main__':
+    main()
