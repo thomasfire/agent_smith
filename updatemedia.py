491fefac980deaa91e18f62fb22d8f582c81e4f4
Thomas <thomasfire@yandex.ru>
2016-06-23 14:59:42 +0300

    Upadates all needed media for using it later and writes it into media.db

---
+++ updatemedia.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+#updates list of available media(files/media.db)
+""" Developer and Author: Thomas Fire https://github.com/thomasfire
+### Main manager: Uliy Bee
+"""
+
+import vk_api
+import re
+
+def updateaudio(vk,mediafile):
+    audlist=vk.audio.get(need_user=1,count=0)
+    newaud=[]
+    for x in audlist['items'][1:]:
+        newaud.append(str(x['owner_id'])+'_'+str(x['id']))
+    mediafile.write('audio:{ '+' '.join(newaud)+' };\n\n')
+
+def updatepics(vk,mediafile,album_id,user_id):
+    piclist=vk.photos.get(owner_id=user_id,album_id=album_id)
+    newpic=[]
+    for x in piclist['items'][1:]:
+        newpic.append(str(x['owner_id'])+'_'+str(x['id']))
+    mediafile.write('photo:{ '+' '.join(newpic)+' };\n\n')
+
+def updategifs(vk,mediafile):
+    giflist=vk.docs.get(type=3)
+    newgif=[]
+    for x in giflist['items'][1:]:
+        newgif.append(str(x['owner_id'])+'_'+str(x['id']))
+    mediafile.write('doc:{ '+' '.join(newgif)+' };\n\n')
+
+def main(vk_session,album_id,user_id):
+        #authorization and getting needable tools
+        try:
+            vk_session.authorization()
+        except vk_api.AuthorizationError as error_msg:
+            print(error_msg)
+            return
+        vk = vk_session.get_api()
+        mediafile=open('files/media.db','w')
+        updateaudio(vk,mediafile)
+        updatepics(vk,mediafile,album_id,user_id)
+        updategifs(vk,mediafile)
+        mediafile.close()
+
+if __name__ == '__main__':
+    #auth
+    vset=open("files/vk.settings","r")
+    settings=vset.read()
+    vset.close()
+    login="".join(re.findall(r"login=(.+)#endlogin",settings))
+    password="".join(re.findall(r"password=(.+)#endpass",settings))
+    chatid=int("".join(re.findall(r"chatid=(\d+)#endchatid",settings)))
+    albumid=int("".join(re.findall(r"album_id=(\d+)#endalbumid",settings)))
+    userid=int("".join(re.findall(r"userid=(\d+)#enduserid",settings)))
+    vk_session = vk_api.VkApi(login, password)
+
+    main(vk_session,albumid,userid)
