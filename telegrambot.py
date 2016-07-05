f433f6cd9514c84a6693eccc445bd89882e7fcc6
Thomas_Fire <thomasfire@yandex.ru>
2016-07-05 18:30:07 +0300

    Some refactor and added Odmin functions

--- telegrambot.py
+++ telegrambot.py
@@ -24,7 +24,8 @@ def makedict():
     f.close()
     ndict={}
     for x in msgs:
-        ndict[x.split(' :: ')[0].strip()]=' : '.join(x.split(' :: ')[1:]).strip()+' ;'
+        currmsg=x.split(' :: ')
+        ndict[currmsg[0].strip()]=' : '.join(currmsg[1:]).strip()+' ;'
     return ndict
 
 
@@ -40,20 +41,22 @@ def updateusers():
     maden=f.read()
     f.close()
     f=open('files/shitlist.db','r')
-    shitlist=f.read().split()
+    shitlist=f.read().strip().split()
     f.close()
     for x in msgs:
-
-        if x.split(' :: ')[0] not in maden and x.split(' :: ')[3][:7]=='/chname':
+        currmsg=x.strip().strip(';').strip().split(' :: ')
+        xcurruser=currmsg[1].strip()
+        if currmsg[0] not in maden and currmsg[3][:7]=='/chname':
             f=open('files/tl_users.db','r')
             users=f.read().strip('@ ').strip(' ;\n').split(' ;\n@ ')
             newusers=[]
             f.close()
             #getting new nickname and writing it to log
             for y in users:
-                if x.split(' :: ')[1].strip()==y.split(' :: ')[0].strip():
-                    newusers.append('  :: '.join([x.split(' :: ')[1],x.split(' :: ')[3][8:].strip().strip(' ;'),y.split(' :: ')[2]]))
-                    tl.sendmsg(url,y.split(' :: ')[0],"The nickname has been changed")
+                ycurruser=y.split(' :: ')[0].strip()
+                if xcurruser==ycurruser:
+                    newusers.append('  :: '.join([xcurruser,currmsg[3][8:].strip().strip(' ;'),y.split(' :: ')[2]]))
+                    tl.sendmsg(url,ycurruser,"The nickname has been changed")
                 else:
                     newusers.append(y)
 
@@ -61,44 +64,47 @@ def updateusers():
             f.write('@ '+' ;\n@ '.join(newusers)+' ;\n')
             f.close()
             f=open('files/tl_msgs.made','a')
-            f.write(' '+x.split(' :: ')[0])
+            f.write(' '+currmsg[0])
             f.close()
 
-        elif (x.split(' :: ')[1] not in shitlist and x.split(' :: ')[0] not in maden and len(x.split(' :: ')[3])<75
-        and len(x.split(' :: ')[3])>60 and x.split(' :: ')[3][:5]=='/auth'):
+        elif (currmsg[1] not in shitlist and currmsg[0] not in maden and len(currmsg[3])<75
+        and len(currmsg[3])>60 and currmsg[3][:5]=='/auth'):
             f=open('files/tokens.db','r')
             tokens=f.read().split('\n')
             f.close()
+
             f=open('files/tl_users.db','r')
             users=f.read()
             f.close()
-            publickey=fcrypto.gethash(x.split(' :: ')[3][6:71].strip())
+
+            publickey=fcrypto.gethash(currmsg[3][6:71].strip())
+
             f=open('files/tl_users.db','a')
+
             q=open('files/tl_tryes.db','r')
             tryusers=q.read()
             q.close()
-            if publickey in tokens and ' '+x.split(' :: ')[1]+' ' not in users:
-                f.write('@ '+x.split(' :: ')[1]+' :: '+'Anonymous'+x.split(' :: ')[1]+' :: '+'all ;')
-                tl.sendmsg(url,x.split(' :: ')[1],"You are now successfully authenticated as "+'Anonymous'+x.split(' :: ')[1]+
+            if publickey in tokens and ' '+currmsg[1]+' ' not in users:
+                f.write('@ '+currmsg[1]+' :: '+'Anonymous'+currmsg[1]+' :: '+'all ;')
+                tl.sendmsg(url,currmsg[1],"You are now successfully authenticated as "+'Anonymous'+currmsg[1]+
                 '.\nYou can change your nicname via /chname <new_nick> or you can view a help message via /help.')
 
                 g=open('files/tokens.db','w')
                 g.write('\n'.join(tokens).replace(publickey+'\n',''))
                 g.close()
-            elif publickey not in tokens and ' '+x.split(' :: ')[1]+' ' not in users:
-                tl.sendmsg(url,x.split(' :: ')[1],'Wrong key.')
-                if '@'+x.split(' :: ')[1]+':' not in tryusers:
-                    tryusers+=' @'+x.split(' :: ')[1]+':1'
+            elif publickey not in tokens and ' '+currmsg[1]+' ' not in users:
+                tl.sendmsg(url,currmsg[1],'Wrong key.')
+                if '@'+currmsg[1]+':' not in tryusers:
+                    tryusers+=' @'+currmsg[1]+':1'
                 else:
                     ntryus=tryusers.split()
                     for y in ntryus:
-                        if '@'+x.split(' :: ')[1]+':' in y:
-                            print(y)
-                            print(y.split(':')[0]+':'+str(int(y.split(':')[1])+1))
+                        curtry=y.strip().split(':')
+                        if '@'+currmsg[1]+':' in y:
                             tryusers=tryusers.replace(y,
-                            y.split(':')[0]+':'+str(int(y.split(':')[1])+1))
-                            if int(y.split(':')[1])>2:
-                                tl.kickuser(x.split(' :: ')[1])
+                            curtry[0]+':'+str(int(curtry[1])+1))
+                            if int(curtry[1])>2:
+                                tl.kickuser(currmsg[1])
                                 tryusers=tryusers.replace(y,'')
                             break
 
@@ -106,24 +112,25 @@ def updateusers():
                 q.write(tryusers)
                 q.close()
             else:
-                tl.sendmsg(url,x.split(' :: ')[1],'You are already logged in. You can change your nick via /chname <new_nick> if you are authenticated.')
+                tl.sendmsg(url,currmsg[1],'You are already logged in. You can change your nick via /chname <new_nick> if you are authenticated.')
             f.close()
             f=open('files/tl_msgs.made','a')
-            f.write(' '+x.split(' :: ')[0])
+            f.write(' '+currmsg[0])
             f.close()
 
-        elif x.split(' :: ')[0] not in maden and x.split(' :: ')[3][:5]=='/mode':
+        elif currmsg[0] not in maden and currmsg[3][:5]=='/mode':
             f=open('files/tl_users.db','r')
             users=f.read().strip('@ ').strip(' ;\n').split(' ;\n@ ')
             newusers=[]
             f.close()
 
             for y in users: #checking if it needable user and if command is correct
-                if (x.split(' :: ')[1].strip()==y.split(' :: ')[0].strip() and
-                x.split(' :: ')[3][6:].strip().strip(' ;') in ['all','no','imnt']):
-                    newusers.append('  :: '.join([x.split(' :: ')[1],y.split(' :: ')[1].strip(),
-                    x.split(' :: ')[3][6:].strip().strip(' ;')]))
-                    tl.sendmsg(url,y.split(' :: ')[0],"The mode has been changed")
+                curruser=y.split(' :: ')
+                if (currmsg[1].strip()==curruser[0].strip() and
+                currmsg[3][6:].strip().strip(' ;') in ['all','no','imnt']):
+                    newusers.append('  :: '.join([currmsg[1],curruser[1].strip(),
+                    currmsg[3][6:].strip().strip(' ;')]))
+                    tl.sendmsg(url,curruser[0],"The mode has been changed")
                 else:
                     newusers.append(y)
             #writing to file and logging the id of the message
@@ -131,21 +138,22 @@ def updateusers():
             f.write('@ '+' ;\n@ '.join(newusers)+' ;\n')
             f.close()
             f=open('files/tl_msgs.made','a')
-            f.write(' '+x.split(' :: ')[0])
+            f.write(' '+currmsg[0])
             f.close()
 
-        elif x.split(' :: ')[0] not in maden and x.split(' :: ')[3][:3]=='/me':
+        elif currmsg[0] not in maden and currmsg[3][:3]=='/me':
             f=open('files/tl_users.db','r')
             users=f.read().strip('@ ').strip(' ;\n').split(' ;\n@ ')
             f.close()
 
             for y in users: #checking if it needable user and if command is correct
-                if x.split(' :: ')[1].strip()==y.split(' :: ')[0].strip():
-                    tl.sendmsg(url,y.split(' :: ')[0],"You are logged in as "+y.split(' :: ')[1]+
-                    ' with recieving '+y.split(' :: ')[2]+' messages.')
+                curruser=y.split(' :: ')
+                if currmsg[1].strip()==curruser[0].strip():
+                    tl.sendmsg(url,curruser[0],"You are logged in as "+curruser[1]+
+                    ' with recieving '+curruser[2]+' messages.')
             #writing to file and logging the id of the message
             f=open('files/tl_msgs.made','a')
-            f.write(' '+x.split(' :: ')[0])
+            f.write(' '+currmsg[0])
             f.close()
 
 
@@ -163,16 +171,18 @@ def makeseq():
     msgs=f.read().split(' ;\n@ ')
     f.close()
     for x in msgs:
-        if x.split(' :: ')[0] not in maden and x.split(' :: ')[3][:4]=='/msg':
+        currmsg=x.strip(';').strip().split(' :: ')
+        if currmsg[0] not in maden and currmsg[3][:4]=='/msg':
             for y in users:
-                if x.split(' :: ')[1].strip()==y.split(' :: ')[0].strip():
+                curruser=y.strip().strip(';').strip().split(' :: ')
+                if currmsg[1].strip()==curruser[0].strip():
                     g=open('files/tl_msgs.seq','a')
-                    g.write('Not_sent_message: '+y.split(' :: ')[1].strip()+': ' +
-                    x.split(' :: ')[3].strip().replace('/msg ',' ').strip(' ;')+' ;\n')
+                    g.write('Not_sent_message: '+curruser[1].strip()+': ' +
+                    currmsg[3].strip().replace('/msg ',' ').strip(' ;')+' ;\n')
                     g.close()
-                    tl.sendmsg(url,y.split(' :: ')[0],"The message will be sent soon.")
+                    tl.sendmsg(url,curruser[0],"The message will be sent soon.")
                     f=open('files/tl_msgs.made','a')
-                    f.write(' '+x.split(' :: ')[0])
+                    f.write(' '+currmsg[0])
                     f.close()
 
 
@@ -193,8 +203,12 @@ def response():
     f=open('files/tl_users.db','r')
     users=f.read()
     f.close()
+    f=open('files/admins.db','r')
+    odmins=f.read().strip().split()
+    f.close()
     for x in msgs:
-        if '@ '+x.split(' :: ')[1].strip()+' ' in users and x.split(' :: ')[0] not in maden and x.split(' :: ')[3][:4]=='/log':
+        currmsg=x.strip(';').strip().split(' :: ')
+        if '@ '+currmsg[1].strip()+' ' in users and currmsg[0] not in maden and currmsg[3][:4]=='/log':
             f=open("files/msgshistory.db","r")
             messages=f.read().strip(' ;').strip("@ ").replace(' :: ',' : ').split(';\n@')
             f.close()
@@ -203,33 +217,57 @@ def response():
             for y in messages:
                 newmsg.append(' : '.join(y.split(' : ')[1:]).strip(' ;'))
             try:
-                tosend='\n'.join(newmsg[-int(x.split(' :: ')[3][5:].strip().strip(' ;')):])
+                tosend='\n'.join(newmsg[-int(currmsg[3][5:].strip().strip(' ;')):])
             except Exception as e:
                 print('Error in making message')
                 tosend='You typed incorrect value. Maybe you requested more messages I have, or you typed non integer number.'
-            tl.sendmsg(url,x.split(' :: ')[1].strip(), tosend)
+            tl.sendmsg(url,currmsg[1].strip(), tosend)
             f=open('files/tl_msgs.made','a')
-            f.write(' '+x.split(' :: ')[0])
+            f.write(' '+currmsg[0])
             f.close()
 
-        elif x.split(' :: ')[0] not in maden and x.split(' :: ')[3][:5]=='/help':
+        elif currmsg[0] not in maden and currmsg[3][:5]=='/help':
             #loading description
             f=open('files/info.db','r')
             helpmsg=f.read().split('VK@@##@@TL')[1].strip()
             f.close()
 
-            tl.sendmsg(url,x.split(' :: ')[1].strip(), helpmsg)
+            tl.sendmsg(url,currmsg[1].strip(), helpmsg)
             f=open('files/tl_msgs.made','a')
-            f.write(' '+x.split(' :: ')[0])
+            f.write(' '+currmsg[0])
             f.close()
 
-        elif '@ '+x.split(' :: ')[1].strip() in users and x.split(' :: ')[0] not in maden and x.split(' :: ')[3][:6]=='/quote':
+        elif '@ '+currmsg[1].strip() in users and currmsg[0] not in maden and currmsg[3][:6]=='/quote':
             f=open('files/citations.db','r')
             tosend=random.choice(f.read().split('\n\n'))
             f.close()
-            tl.sendmsg(url,x.split(' :: ')[1].strip(), tosend)
+            tl.sendmsg(url,currmsg[1].strip(), tosend)
             f=open('files/tl_msgs.made','a')
-            f.write(' '+x.split(' :: ')[0])
+            f.write(' '+currmsg[0])
+            f.close()
+
+        elif currmsg[1].strip() in odmins and currmsg[0] not in maden and currmsg[3][:6]=='/tllog':
+            f=open("files/tl_msgs.db","r")
+            messages=f.read().strip(' ;').strip("@ ").replace(' :: ',' : ').split(';\n@')
+            f.close()
+            newmsg=[]
+
+            for y in messages:
+                newmsg.append(' : '.join(y.split(' : ')[1:]).strip(' ;'))
+            try:
+                tosend='\n'.join(newmsg[-int(currmsg[3][7:].strip().strip(' ;')):])
+            except Exception as e:
+                print('Error in making message')
+                tosend='You typed incorrect value. Maybe you requested more messages I have, or you typed non integer number.'
+            tl.sendmsg(url,currmsg[1].strip(), tosend)
+            f=open('files/tl_msgs.made','a')
+            f.write(' '+currmsg[0])
+            f.close()
+
+        elif currmsg[1].strip() in odmins and currmsg[0] not in maden and currmsg[3][:8]=='/tlusers':
+            tl.sendmsg(url,currmsg[1].strip(), users)
+            f=open('files/tl_msgs.made','a')
+            f.write(' '+currmsg[0])
             f.close()
 
 
@@ -260,12 +298,15 @@ def fromvktotl():
         users=f.read().strip('@').strip(';').strip().split(' ;\n@ ')
         f.close()
         for x in users:
-            if x.split(' :: ')[2].strip()=='no':
+            curruser=x.strip('@').strip(';').strip().split(' :: ')
+            usermode,userid=curruser[2].strip(),curruser[0].strip()
+
+            if usermode=='no':
                 continue
-            elif toimnt and x.split(' :: ')[2].strip().strip(' ;')=='imnt':
-                tl.sendmsg(url,x.split(' :: ')[0].strip(), toimnt)
-            elif x.split(' :: ')[2].strip().strip(' ;')=='all':
-                tl.sendmsg(url,x.split(' :: ')[0].strip(), toall)
+            elif toimnt and usermode=='imnt':
+                tl.sendmsg(url,userid, toimnt)
+            elif usermode=='all':
+                tl.sendmsg(url,userid, toall)
 
     f=open('files/msgs.sent','a')
     f.write(' '+' '.join(allmsg))
