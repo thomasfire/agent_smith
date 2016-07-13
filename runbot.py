79a791661713ba82d3f3a6317f332ee70ea30c75
Thomas_Fire <thomasfire@yandex.ru>
2016-07-13 12:21:49 +0300

    Successfully tested

--- runbot.py
+++ runbot.py
@@ -27,72 +27,72 @@ lastid=0
 url=''
 
 def captcha_handler(captcha):
-    global lastid
-    key = getcaptcha(url,captcha.get_url().strip(),lastid).strip(';').strip()
-    return captcha.try_again(key)
+	global lastid
+	key = getcaptcha(url,captcha.get_url().strip(),lastid).strip(';').strip()
+	return captcha.try_again(key)
 
 def clearsent():
-    f=open('files/msgs.sent','r')
-    sent=f.read().split()
-    f.close()
-    f=open('files/msgs.sent','w')
-    f.write(' '.join(sent))
-    f.close()
+	f=open('files/msgs.sent','r')
+	sent=f.read().split()
+	f.close()
+	f=open('files/msgs.sent','w')
+	f.write(' '.join(sent))
+	f.close()
 
 def main():
-    psswd=gethash(getpass(),mode='pass')
-    settings=fdecrypt("files/vk.settings",psswd)
-    login="".join(re.findall(r"login=(.+)#endlogin",settings))
-    password="".join(re.findall(r"password=(.+)#endpass",settings))
-    chatid=int("".join(re.findall(r"chatid=(\d+)#endchatid",settings)))
-    albumid=int("".join(re.findall(r"album_id=(\d+)#endalbumid",settings)))
-    userid=int("".join(re.findall(r"userid=(\d+)#enduserid",settings)))
+	psswd=gethash(getpass(),mode='pass')
+	settings=fdecrypt("files/vk.settings",psswd)
+	login="".join(re.findall(r"login=(.+)#endlogin",settings))
+	password="".join(re.findall(r"password=(.+)#endpass",settings))
+	chatid=int("".join(re.findall(r"chatid=(\d+)#endchatid",settings)))
+	albumid=int("".join(re.findall(r"album_id=(\d+)#endalbumid",settings)))
+	userid=int("".join(re.findall(r"userid=(\d+)#enduserid",settings)))
 
-    try:
-        vk_session = vk_api.VkApi(login, password,captcha_handler=captcha_handler)
-    except:
-        exception('smth goes wrong at getting vk_session')
+	try:
+		vk_session = vk_api.VkApi(login, password,captcha_handler=captcha_handler)
+	except:
+		exception('smth goes wrong at getting vk_session')
 
-    #authorization
-    try:
-        vk_session.authorization()
-    except vk_api.AuthorizationError as error_msg:
-        exception(error_msg)
-        return
-    try:
-        vk = vk_session.get_api()
-    except Exception as e:
-        exception('smth goes wrong at geting api\n')
+	#authorization
+	try:
+		vk_session.authorization()
+	except vk_api.AuthorizationError as error_msg:
+		exception(error_msg)
+		return
+	try:
+		vk = vk_session.get_api()
+	except Exception as e:
+		exception('smth goes wrong at geting api\n')
 
-    #getting url
-    global url
-    url=geturl(psswd)
+	#getting url
+	global url
+	url=geturl(psswd)
 
-    cycles=0
-    global lastid
-    tllast=0
-    print('Logged in, starting bot...')
-    while True:
-        try:
-            if cycles%3==0:
-                print('.',end='')
-                stdout.flush()
-            lastid=getmsg.main(vk,chatid,lastid)
-            if cycles>=500:
-                updatemedia.main(vk,albumid,userid)
-                cycles=0
-                print('\n',str(datetime.now()),':  Big cycle done!;    vklast=',lastid,';  tllast=',tllast)
-            makeseq.main()
-            tllast=telegrambot.main(psswd,url,tllast)
-            sendtovk.main(vk,chatid)
-            clearsent()
-            cycles+=1
-        except Exception as exp:
-            exception("Something gone wrong in bot:\n")
+	cycles=0
+	global lastid
+	tllast=0
+	print('Logged in, starting bot...')
+	while True:
+		try:
+			if cycles%3==0:
+				print('.',end='')
+				stdout.flush()
+			lastid=getmsg.main(vk,chatid,lastid)
+			if cycles>=500:
+				updatemedia.main(vk,albumid,userid)
+				cycles=0
+				print('\n',str(datetime.now()),':  Big cycle done!;    vklast=',lastid,';  tllast=',tllast)
+			makeseq.main()
+			tllast=telegrambot.main(psswd,url,tllast)
+			sendtovk.main(vk,chatid)
+			clearsent()
+			cycles+=1
+		except Exception as exp:
+			exception("Something gone wrong in bot:\n")
 
 
 
 
 
 if __name__ == '__main__':
-    main()
+	main()
