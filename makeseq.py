70c0a82edece3b8a44241f88edb764a5990e2948
Thomas_Fire <thomasfire@yandex.ru>
2016-11-13 23:20:36 +0300

    Refactored

--- makeseq.py
+++ makeseq.py
@@ -6,61 +6,34 @@
 ### Main manager: Uliy Bee
 """
 
-from re import findall
 
 
 
-
-
-def mkmain(msghistory, vk_msgs, sent_msgs, new_to_tl):
-	listofmsgs=msghistory.read().split(' ;\n@ ')[-200:]
-
-	f=open('files/keywords.db','r')
-	keywords=f.read().split()
+def load_keywords():
+	f = open('files/keywords.db','r')
+	keywords = f.read().split()
 	f.close()
 
+	return keywords
+
+
+
+
+
+def mkmain(message, keywords, list_of_alles, list_of_imnts, list_of_cmds):
 	impnt=[]
 	allmsg=[]
 
-	# making it more readable
-	for x in range(len(listofmsgs)):
-		listofmsgs[x]=listofmsgs[x].strip('@ ').strip(' ;')
-
-	# loading sent messages
-	sent = sent_msgs.read().split()
-
-	# cycling through messages
-	for x in listofmsgs:
-		msg=x.split(' :: ')
-		mess=True
-		if len(msg)<4: # some messages maybe empty. it is normal.
-			continue
-		# not to send message if message consists commands
-		for y in ['/quote','/music','/gif','/info','/pic']:
-			if y in msg[3]:
-				mess=False
-				break
-		if not mess: continue
-
-		# making all and imnt sequances
-		if msg[0].strip() not in sent:
-			allmsg.append(msg[0].strip())
-			# cycling through keywords
-			for y in keywords:
-				if y in msg[3].lower() or (msg[3]==msg[3].upper() and len(findall(r'[A-Za-zА-Яа-я]?',msg[3]))>=3):
-					impnt.append(msg[0].strip())
-					break
-	# writing sequances
-	if allmsg:
-		new_to_tl.value = 1
-	vk_msgs.write('w', "important:{"+" ".join(impnt)+"}\n\n" +"all:{"+" ".join(allmsg)+"}")
-	# cleaning up messages
-	if len(listofmsgs)>1000:
-		msghistory.write('w', '@ '+' ;\n@ '.join(listofmsgs[-100:]))
+	# not to send message if message consists commands
+	for y in ['/quote', '/music', '/gif', '/info', '/pic']:
+		if y in message[3]:
+			list_of_cmds.append(message)
+			return
 
 
-
-
-
-if __name__ == '__main__':
-	main()
+	list_of_alles.append(message)
+	# cycling through keywords
+	for y in keywords:
+		if y in message[3].lower() or message[3]==message[3].upper():
+			list_of_imnts.append(message)
+			return
